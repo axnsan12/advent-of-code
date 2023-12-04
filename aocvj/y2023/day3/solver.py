77a84614ae1844022d6caceee1ce40d739fcd660
Cristi Vîjdea <cristi@cvjd.me>
2023-12-04 03:46:43 +0200

    Solved day 3

--- aocvj/y2023/day3/solver.py
+++ aocvj/y2023/day3/solver.py
@@ -10,16 +10,29 @@ def solve(data: str) -> tuple[int | str, int | str | None]:
 
     answer_a = 0
 
-    for (before, line, after) in more_itertools.windowed(lines, 3):
-        for m in re.finditer(r'\d+', line):
+    part_numbers = {}
+    for window in more_itertools.windowed(enumerate(lines), 3):
+        window: tuple[tuple[int, str], ...]
+        for m in re.finditer(r'\d+', window[1][1]):
+            part_number = int(m.group(0))
             is_part = False
-            for ln in (before, line, after):
+            for (line_idx, ln) in window:
                 for idx in range(m.start() - 1, m.end() + 1):
                     if ln[idx] != '.' and not ln[idx].isdigit():
                         is_part = True
+                        part_id = (line_idx, idx)
+
+                        check, numbers = part_numbers.setdefault(part_id, (ln[idx], []))
+                        assert check == ln[idx]
+                        numbers.append(part_number)
                         break
 
             if is_part:
                 answer_a += int(m.group(0))
 
-    return answer_a, None
+    answer_b = 0
+    for (part_id, (part_char, part_numbers)) in part_numbers.items():
+        if part_char == '*' and len(part_numbers) == 2:
+            answer_b += part_numbers[0] * part_numbers[1]
+
+    return answer_a, answer_b
