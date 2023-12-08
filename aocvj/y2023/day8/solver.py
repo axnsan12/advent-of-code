d7100c21ab5c103205a3aeb2f4a3ef5e96b9d873
Cristi Vîjdea <cristi@cvjd.me>
2023-12-08 12:39:39 +0200

    Solved 2023/08 part 2

--- aocvj/y2023/day8/solver.py
+++ aocvj/y2023/day8/solver.py
@@ -1,4 +1,18 @@
 import itertools
+import math
+from typing import Callable
+
+
+def solve_for_nodes(graph: dict[str, dict[str, str]], instructions: str,
+                    nodes: list[str], goal_fn: Callable[[str], bool]) -> tuple[list[str], int]:
+    count = 0
+    for step in itertools.cycle(instructions):
+        count += 1
+        nodes = [graph[node][step] for node in nodes]
+        if all(goal_fn(node) for node in nodes):
+            break
+
+    return nodes, count
 
 
 def solve(data: str) -> tuple[int | str, int | str | None]:
@@ -7,16 +21,22 @@ def solve(data: str) -> tuple[int | str, int | str | None]:
 
     graph = {}
     for ln in lines[1:]:
-        letters = ''.join(c for c in ln if c.isalpha())
+        letters = ''.join(c for c in ln if c.isalnum())
         src, left, right = letters[:3], letters[3:6], letters[6:]
         graph[src] = {'L': left, 'R': right}
 
-    node = 'AAA'
-    answer_a = 0
-    for step in itertools.cycle(instructions):
-        answer_a += 1
-        node = graph[node][step]
-        if node == 'ZZZ':
-            break
+    if 'AAA' in graph:
+        _, answer_a = solve_for_nodes(graph, instructions, ['AAA'], lambda n: n == 'ZZZ')
+    else:
+        answer_a = None
 
-    return answer_a, None
+    start_nodes = [n for n in graph if n.endswith('A')]
+    distances = set()
+    for node in start_nodes:
+        [goal], distance = solve_for_nodes(graph, instructions, [node], lambda n: n.endswith('Z'))
+        _, check_distance = solve_for_nodes(graph, instructions, [goal], lambda n: n == goal)
+        assert distance == check_distance, f'wtf??? {distance=} {check_distance=}'
+        distances.add(distance)
+
+    answer_b = math.lcm(*distances)
+    return answer_a, answer_b
