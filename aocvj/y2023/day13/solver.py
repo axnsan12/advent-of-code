78e22457f2628e691a688daac4c9509d1b173544
Cristi Vîjdea <cristi@cvjd.me>
2023-12-13 14:26:14 +0200

    Solved 2023/13 part 2

--- aocvj/y2023/day13/solver.py
+++ aocvj/y2023/day13/solver.py
@@ -3,17 +3,33 @@ import itertools
 import numpy as np
 
 
-def find_reflection(grid: np.ndarray) -> int:
+def find_reflection(grid: np.ndarray, try_fix: int = None) -> int:
     for candidate_idx in range(1, len(grid)):
         i = candidate_idx - 1
         j = candidate_idx
+        applied_fix = False
+
         while i >= 0 and j < len(grid):
             if not np.array_equal(grid[i], grid[j]):
+                if try_fix is not None and not applied_fix:
+                    # if there is one single difference, we can "fix" (ignore) it
+                    if np.sum(grid[i] != grid[j]) == 1:
+                        applied_fix = True
+                        i -= 1
+                        j += 1
+                        continue
+
                 break
 
             i -= 1
             j += 1
         else:
+            if candidate_idx == try_fix:
+                # we are trying to find an "imperfect" match (for part 2)
+                # so skip the perfect match
+                assert not applied_fix, f'wtf?? {try_fix} was supposed to be perfect!'
+                continue
+
             return candidate_idx
 
     return 0
@@ -21,7 +37,9 @@ def find_reflection(grid: np.ndarray) -> int:
 
 def solve(data: str) -> tuple[int | str, int | str | None]:
     lines = iter(data.splitlines(keepends=False))
+
     answer_a = 0
+    answer_b = 0
     while True:
         grid = []
         for ln in lines:
@@ -39,4 +57,8 @@ def solve(data: str) -> tuple[int | str, int | str | None]:
         vertical = find_reflection(grid.T)
         answer_a += horizontal * 100 + vertical
 
-    return answer_a, None
+        horizontal_fixed = find_reflection(grid, try_fix=horizontal)
+        vertical_fixed = find_reflection(grid.T, try_fix=vertical)
+        answer_b += horizontal_fixed * 100 + vertical_fixed
+
+    return answer_a, answer_b
