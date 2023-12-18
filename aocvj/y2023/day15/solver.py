33bd0847085f5f6ab8028fe7dd4f5ae345cd33b6
Cristi Vîjdea <cristi@cvjd.me>
2023-12-18 22:26:54 +0200

    Solved 2023/15 part 2

--- aocvj/y2023/day15/solver.py
+++ aocvj/y2023/day15/solver.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def compute_hash(s: str) -> int:
     h = 0
     for c in s:
@@ -12,6 +15,21 @@ def solve(data: str) -> tuple[int | str, int | str | None]:
     compute_hash("HASH")
 
     answer_a = 0
+
+    boxes = defaultdict(dict)
     for step in data.strip().split(','):
         answer_a += compute_hash(step)
-    return answer_a, None
+
+        if step.endswith('-'):
+            step = step[:-1]
+            boxes[compute_hash(step)].pop(step, None)
+        else:
+            step, val = step.split('=')
+            boxes[compute_hash(step)][step] = int(val)
+
+    answer_b = 0
+    for box, lenses in sorted(boxes.items()):
+        for j, (lens, val) in enumerate(lenses.items(), start=1):
+            answer_b += (box + 1) * j * val
+
+    return answer_a, answer_b
