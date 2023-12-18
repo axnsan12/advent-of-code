d171c8cb251d6d2bc33bf99ced86ee030ce8a032
Cristi Vîjdea <cristi@cvjd.me>
2023-12-18 22:18:56 +0200

    Solved 2023/15 part 1

--- aocvj/y2023/day15/solver.py
+++ aocvj/y2023/day15/solver.py
@@ -1,2 +1,17 @@
+def compute_hash(s: str) -> int:
+    h = 0
+    for c in s:
+        h += ord(c)
+        h *= 17
+        h %= 256
+
+    return h
+
+
 def solve(data: str) -> tuple[int | str, int | str | None]:
-    return "", None
+    compute_hash("HASH")
+
+    answer_a = 0
+    for step in data.strip().split(','):
+        answer_a += compute_hash(step)
+    return answer_a, None
