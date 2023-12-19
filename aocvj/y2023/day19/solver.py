4be0da9c90c9478cb20aa358635e818210f42c35
Cristi Vîjdea <cristi@cvjd.me>
2023-12-19 13:44:36 +0200

    Solved 2023/19 part 1

---
+++ aocvj/y2023/day19/solver.py
@@ -0,0 +1,72 @@
+import dataclasses
+import operator
+import re
+from typing import Callable
+
+
+@dataclasses.dataclass(frozen=True)
+class Part:
+    x: int
+    m: int
+    a: int
+    s: int
+
+
+def parse_condition(condition: str) -> Callable[[Part], bool]:
+    ops = [
+        ('<', operator.lt),
+        ('>', operator.gt),
+    ]
+
+    for op_str, op_fn in ops:
+        if op_str in condition:
+            attr_name, value = condition.split(op_str)
+            value = int(value)
+            return lambda part: op_fn(getattr(part, attr_name), value)
+
+    raise ValueError(f'unknown condition: {condition}')
+
+
+def solve(data: str) -> tuple[int | str, int | str | None]:
+    lines = iter(data.splitlines(keepends=False))
+
+    workflows = {}  # type: dict[str, list[tuple[str, Callable[[Part], bool]]]]
+    for ln in lines:
+        if not ln:
+            break
+
+        state, rules = re.match(r"^(.*?)\{(.*?)}$", ln).groups()
+        flow = []
+        for rule in rules.split(','):
+            if ':' in rule:
+                condition, target = rule.split(':')
+                flow.append((target, parse_condition(condition)))
+            else:
+                flow.append((rule, lambda _: True))
+
+        workflows[state] = flow
+
+    parts = []
+    for ln in lines:
+        attrs = {}
+        for a in ln[1:-1].split(','):
+            k, v = a.split('=')
+            attrs[k] = int(v)
+
+        parts.append(Part(**attrs))
+
+    answer_a = 0
+    for part in parts:
+        state = 'in'
+        while state not in ('A', 'R'):
+            for target, condition in workflows[state]:
+                if condition(part):
+                    state = target
+                    break
+            else:
+                raise AssertionError(f'{part} stuck in {state} {workflows[state]}')
+
+        if state == 'A':
+            answer_a += part.x + part.m + part.a + part.s
+
+    return answer_a, None
