657be503d111851ba3313eb95babdfac28d3cf25
Cristi Vîjdea <cristi@cvjd.me>
2024-12-04 02:30:27 +0200

    Solved 2024/03 part 1

--- aocvj/y2024/day3/solver.py
+++ aocvj/y2024/day3/solver.py
@@ -1,2 +1,7 @@
+import re
+
+
 def solve(data: str) -> tuple[int | str, int | str | None]:
-    return "", None
+    mul_re = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+    answer_a = sum(int(x) * int(y) for x, y in mul_re.findall(data))
+    return answer_a, None
