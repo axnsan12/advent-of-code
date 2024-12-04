06ab05aeba83c06f791f3bfe85ed2703c5fe1504
Cristi Vîjdea <cristi@cvjd.me>
2024-12-04 02:14:42 +0200

    Solved 2024/02 part 2

--- aocvj/y2024/day2/solver.py
+++ aocvj/y2024/day2/solver.py
@@ -1,12 +1,21 @@
 import itertools
 
 
+def is_safe(numbers: list[int]) -> bool:
+    diffs = [a - b for a, b in itertools.pairwise(numbers)]
+    sign = diffs[0] < 0
+    return all(((v < 0) == sign) and (1 <= abs(v) <= 3) for v in diffs)
+
+
 def solve(data: str) -> tuple[int | str, int | str | None]:
     answer_a = 0
+    answer_b = 0
     for line in data.splitlines():
         numbers = list(map(int, line.split()))
-        diffs = [a - b for a, b in itertools.pairwise(numbers)]
-        sign = diffs[0] < 0
-        if all(((v < 0) == sign) and (1 <= abs(v) <= 3) for v in diffs):
+        if is_safe(numbers):
             answer_a += 1
-    return answer_a, None
+        for idx in range(len(numbers)):
+            if is_safe(numbers[:idx] + numbers[idx + 1:]):
+                answer_b += 1
+                break
+    return answer_a, answer_b
