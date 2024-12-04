9a574cca2c91a1bcd60e86307ae568bb709b40b9
Cristi Vîjdea <cristi@cvjd.me>
2024-12-04 02:06:09 +0200

    Solved 2024/01 part 2

--- aocvj/y2024/day1/solver.py
+++ aocvj/y2024/day1/solver.py
@@ -1,6 +1,12 @@
+from collections import Counter
+
+
 def solve(data: str) -> tuple[int | str, int | str | None]:
     left, right = zip(*(map(int, line.split()) for line in data.splitlines()))
     left = sorted(left)
     right = sorted(right)
     answer_a = sum(abs(x - y) for x, y in zip(left, right))
-    return answer_a, None
+
+    right_counts = Counter(right)
+    answer_b = sum(x * right_counts[x] for x in left)
+    return answer_a, answer_b
