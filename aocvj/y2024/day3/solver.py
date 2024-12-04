86b310ea17d53a2fac86a25777508239a71035fb
Cristi Vîjdea <cristi@cvjd.me>
2024-12-04 02:41:20 +0200

    Solved 2024/03 part 2

--- aocvj/y2024/day3/solver.py
+++ aocvj/y2024/day3/solver.py
@@ -2,6 +2,18 @@ import re
 
 
 def solve(data: str) -> tuple[int | str, int | str | None]:
-    mul_re = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
-    answer_a = sum(int(x) * int(y) for x, y in mul_re.findall(data))
-    return answer_a, None
+    mul_re = re.compile(r"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)")
+    answer_a = 0
+    answer_b = 0
+    dont = False
+    for op in mul_re.finditer(data):
+        if op.group(0) == "do()":
+            dont = False
+        elif op.group(0) == "don't()":
+            dont = True
+        else:
+            x, y = map(int, op.groups())
+            answer_a += x * y
+            if not dont:
+                answer_b += x * y
+    return answer_a, answer_b
