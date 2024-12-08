f07c93a7f9b80f2205e367e74e274e264183a199
Cristi Vîjdea <cristi@cvjd.me>
2024-12-09 01:46:30 +0200

    Solved 2024/07 part 2

--- aocvj/y2024/day7/solver.py
+++ aocvj/y2024/day7/solver.py
@@ -3,21 +3,27 @@ import itertools
 
 def solve(data: str) -> tuple[int | str, int | str | None]:
     answer_a = 0
+    answer_b = 0
+
     for line in data.splitlines(keepends=False):
         result, operands = line.split(": ")
         result = int(result)
         operands = list(map(int, operands.split()))
 
-        for operators in itertools.product(["+", "*"], repeat=len(operands) - 1):
+        for operators in itertools.product(["+", "*", "|"], repeat=len(operands) - 1):
             computed_result = operands[0]
             for operator, operand in zip(operators, operands[1:]):
                 if operator == "+":
                     computed_result += operand
-                else:
+                elif operator == "*":
                     computed_result *= operand
+                elif operator == "|":
+                    computed_result = int(str(computed_result) + str(operand))
 
             if computed_result == result:
-                answer_a += result
+                answer_b += result
+                if "|" not in operators:
+                    answer_a += result
                 break
 
-    return answer_a, None
+    return answer_a, answer_b
